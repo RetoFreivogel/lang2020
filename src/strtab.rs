561afeee0615c139edc628cc4bfb70e1a831fe98
Reto Freivogel <rfreivogel@windowslive.com>
2020-12-06 16:52:28 +0100

    made Ident use a Index

--- src/strtab.rs
+++ src/strtab.rs
@@ -1,35 +1,31 @@
 use std::sync::RwLock;
 use lazy_static::lazy_static;
-use std::sync::Arc;
+
 use std::fmt::{Display, Result, Formatter};
 
-#[derive(Debug, Clone)]
-pub struct Ident(Arc<String>);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Ident(usize);
 
 lazy_static!{
-    static ref WORDS: RwLock<Vec<Ident>> = RwLock::new(Vec::new());
+    static ref WORDS: RwLock<Vec<String>> = RwLock::new(Vec::new());
 }
 
-impl PartialEq for Ident{
-    fn eq(&self, other: &Self) -> bool{
-        Arc::eq(&self.0, &other.0)
-    }
-}
-impl Eq for Ident{}
 impl Display for Ident{
     fn fmt(&self, f: &mut Formatter) -> Result{
-        self.0.fmt(f)
+        WORDS.read().unwrap()[self.0].fmt(f)
     }
 }
+
 impl Ident {
     pub fn ident(s: &str) -> Ident {
-        for word in WORDS.read().unwrap().iter() {
-            if *word.0 == s {
-                return word.clone();
+        for (i, word) in WORDS.read().unwrap().iter().enumerate() {
+            if word == s {
+                return Ident(i);
             }
         }
-        let word = Ident(Arc::new(s.to_string()));
-        WORDS.write().unwrap().push(word.clone());
+        let word = Ident(WORDS.read().unwrap().len());
+        WORDS.write().unwrap().push(s.to_string());
         return word;
     }
-}   
+}
+   
